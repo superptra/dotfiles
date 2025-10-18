726daa956af2d605b0e153c21558e574eb46f290
pixie <megcd93@gmail.com>
2025-10-18 15:25:29 -0600

    dirs for undo

---
+++ README.md
@@ -0,0 +1,6 @@
+README file for my dotfiles
+==========================
+
+* Vi
+
+Make sure ~/.vim/backup and ~/.vim/undo exist
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir ~/.vim/undo
+mkdir ~/.vim/backup
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
